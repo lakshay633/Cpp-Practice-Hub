bb69f02fb25d28187229c81cdaf412cf3af19b2b
Lakshay Agarwal <633lakshay@gmail.com>
2023-07-03 08:26:15 +0530

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,10 @@
+// C++ Program To Print Ascii Value Of Character.
+
+#include <iostream>
+using namespace std;
+int main(){
+    char a;
+    cin >>a ;
+    cout<<"the ascii value of char is "<<int(a);
+    return 0;
+}
