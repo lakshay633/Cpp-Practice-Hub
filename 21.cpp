d14de2be0217618e29606536c956cec48fd23f04
Lakshay Agarwal <633lakshay@gmail.com>
2023-07-05 05:44:43 +0530

    Add files via upload

---
+++ 21.cpp
@@ -0,0 +1,12 @@
+// C++ Program To Display Size Of Different Datatype.
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"size of char "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"size of int "<<sizeof(a)<<" bytes"<<endl;
+    cout<<"size of float "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"size of double "<<sizeof(double)<<" bytes"<<endl;
+    return 0;
+}
